714715a0a6e9f4b05ca674c264320a88739f9018
Hao__ <jianghao012@foxmail.com>
2023-07-07 15:46:48 +0800

    封装 MinIO 为 starter

---
+++ heima-leadnews-test/minio-demo/src/main/java/com/heima/minio/MinIOApplication.java
@@ -0,0 +1,19 @@
+package com.heima.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: Todo
+ * Class Name: MinIOApplication
+ * Date: 2023/7/7 15:25
+ *
+ * @author Hao
+ * @version 1.1
+ */
+@SpringBootApplication
+public class MinIOApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinIOApplication.class, args);
+    }
+}
